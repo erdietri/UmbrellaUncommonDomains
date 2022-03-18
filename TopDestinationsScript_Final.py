e66ecef1101616dd0effb228bd01c1a8256aebdc
Erika Dietrick (erdietri) <erdietri@cisco.com>
2022-03-18 16:27:47 -0400

    Everything works now until write_top_destinations(response_data). It is grabbing the domains, and it is writing the file, but it's not writing the resulting file correctly - the formatting is weird.

--- TopDestinationsScript_Final.py
+++ TopDestinationsScript_Final.py
@@ -5,10 +5,10 @@ from time import time
 import zipfile
 import os.path
 
-organization_id = ""
+organization_id = "7949881"
 destinations_url = "https://reports.api.umbrella.com/v2/organizations/" + organization_id + "/top-destinations/dns"
-client_id = ""
-client_secret = ""
+client_id = "691080aa96d44bd6bec90fb846b3d2d1"
+client_secret = "0cf486104a3f4f8799a37c19615b8c23"
 token_url = "https://management.api.umbrella.com/auth/v2/oauth2/token"
 
 # Retrieve access_token from Umbrella Management API; lasts 1 hour.
@@ -40,13 +40,14 @@ def get_top_destinations(access_token):
     }
     
     top_destinations = requests.get(destinations_url, headers=headers,params=params)
-    response_data = top_destinations.json()    
+    response_data = top_destinations.json()  
+    print(response_data)  
     return response_data
         
 # Write each domain in Top Destinations as a new line in a CSV.
 def write_top_destinations(response_data):
 
-        csvfile = open('dest_list.csv', 'w')
+        csvfile = open('C:\\dest_list.csv', 'w')
         filewriter = csv.writer(csvfile, delimiter=',',
                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for domain in response_data['data']: 
@@ -56,34 +57,36 @@ def write_top_destinations(response_data):
         
         return csvfile
             
-# Download the Umbrella top 1 million destinations. 
+# Download the Umbrella top 1 million destinations to C: and unzip file. 
 def get_top_million(): 
-    file_path = 'C:\\Users\\erdietri\\Downloads\\'
+    file_path = 'C:\\'
     get_file = requests.get("http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip")
     top_million = os.path.join(file_path, 'top-1m.csv.zip')
     
-    ##################################
-    ##################################
-    # How do I figure out how to save the file to my desktop so that I can unzip it???
-    with zipfile.ZipFile('C:\\Users\\erdietri\\Downloads\\top-1m.csv.zip', 'r') as zip_ref: 
-        zip_ref.extractall('C:\\Users\\erdietri\\Downloads')
-    
+    with open(top_million, 'wb') as f: 
+        f.write(get_file.content)
+        
+    with zipfile.ZipFile(top_million, 'r') as zip_ref: 
+        zip_ref.extractall(file_path)
+
     return top_million       
     
 # Iterates over the top 1 million csv_file to determine if any Top Destinations do not match; returns those that do not match.
-def iterator(csvfile, top_million):
-    with open ("dest_list.csv", 'r') as f1, open("top_million", 'r') as f2:
+def iterator(f1, f2):
+    with open ("dest_list.csv", 'r') as f1, open("top-1m.csv", 'r') as f2:
         fileone = f1.readlines()
         filetwo = f2.readlines() 
-    with open("differences.csv", 'w') as output: 
+    with open("C:\\differences.csv", 'w') as output: 
         for line in fileone: 
             if line not in filetwo: 
-                output.write(line)        
+                output.write(line)  
+    #file_path = 'C:\\differences.csv'
+    #differences = os.path.join(file_path, 'differences.csv')
     print(output)
     return output
 
 top_million = get_top_million()
-#access_token = get_access_token()
-#response_data = get_top_destinations(access_token)
-#csvfile = write_top_destinations(response_data)
-#output = iterator(csvfile,top_million)
+access_token = get_access_token()
+response_data = get_top_destinations(access_token)
+csvfile = write_top_destinations(response_data)
+#output = iterator(f1="C:\\dest_list.csv",f2="C:\\top-1m.csv")
