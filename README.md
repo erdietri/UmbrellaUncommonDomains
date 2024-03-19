bd7118047a8994c54b4ec5db229a120d9eeaf63b
Erika Dietrick (erdietri) <erdietri@cisco.com>
2024-03-19 18:28:18 -0400

    Recommitting code after hard reset. Script tested on both Windows and Mac and successfully finds uncommon domains. Requirements file with dependencies, updated README, and .env file included.

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
-# UmbrellaTop1M
-This script is for Cisco Umbrella customers tasked with better understanding their DNS requests. The script retrieves your Top Destinations over the past week and compares it to Umbrella's Top 1 Million Domains, exporting a CSV of the overlap. 
+# UmbrellaUncommonDomains
+This script allows Cisco Umbrella users to better understand their DNS traffic. The script retrieves the Top Destinations reported by your Umbrella account over the past week, filters for domains only (no IP addresses), and compares it to Umbrella's Top 1 Million Domains. The result is a CSV containing "uncommon domains" in your network, aka, DNS requests in your network that are not part of Umbrella's Top 1 Million. 
 
-## Roadmap
-This is a work in progress. Script finished/tested for Windows, but will be modified/tested to work with other OS for portability.
+## Audience
+* Cisco Umbrella users (customers)
+* Cisco DevNet community (sandbox users)
+
+## Requirements
+* Access to active Cisco Umbrella account OR Cisco DevNet Umbrella sandbox:
+* Umbrella Admin API key with minimum scope of Destinations: Read-Only
+* VS Code 2022 (or IDE of your choice)
+* Python 3.12.1
+* Python libraries listed in '''requirements.txt''' (see below, Run Project, to install)
+
+## Installation & Usage
+### Install VS Code & Python
+* If you do not have VS Code or another IDE, you may download and install VS Code for your OS here: ```https://code.visualstudio.com/download```.
+* If you do not have Python3, choose your OS and download python here: ```https://www.python.org/downloads/```. Then, follow the installation instructions here based on your OS: ```https://kinsta.com/knowledgebase/install-python/#how-to-install-python```.
+
+### Create Umbrella Admin API Key
+1. Login to the Umbrella dashboard:
+ * Umbrella customers: ```https://login.umbrella.com/```
+ * Umbrella sandbox users: ```https://devnetsandbox.cisco.com/DevNet/catalog/umbrella-secure-internet-gateway```
+2. In the leftside menu, navigate to Admin > API Keys
+3. Click the plus symbol in the top right of the screen that says “Add.”
+4. For the Key Scope, at a minimum, you’ll need to check Reports > Aggregation with Read-Only Access.
+5. Choose a date that isn't today for the API expiration date (or click “Never expires”).
+6. Save the generated API Key and Key Secret, as we will be using this in the project.
+    
+### Run Project
+* Clone this repository:
+```git clone https://github.com/erdietri/UncommonDNSRequests.git```
+* Navigate to the project directory:
+```cd umbrellauncommondomains```
+* Install the dependencies:
+```pip install -r requirements.txt```
+* Add your API Key and Key Secret values to the .env file.
+* Run the Python script:
+```python3 umbrella_uncommon_domains.py```
+* Upon completion, the terminal will display the message "Uncommon domains have been written to uncommon_domains.csv in your current directory." You should see a CSV named uncommon_domains.csv in the same directory, which you can open to see your network's uncommon domains.
+
+### Testing
+This script has been tested on Windows 11 and Sanoma 14.3 (Mac) but should work on any OS.
+
+### Questions, Bugs, Feature Requests
+Email erikathedev@gmail.com.
+
---
+++ umbrella_uncommon_domains.py
@@ -0,0 +1,149 @@
+import requests
+import csv
+import zipfile
+import pandas
+from dotenv import load_dotenv 
+import os
+import pandas
+import zipfile
+from IPy import IP
+
+# This script is written under the assumption that it will be run on a weekly basis.
+# Access token generation also requires that the user has a valid Umbrella API Key and Secret: https://developer.cisco.com/docs/cloud-security/#!authentication/manage-api-keys
+
+load_dotenv()
+
+# Environmental variables should contain your org's values in .env file.
+client_key = os.environ['API_KEY']
+client_secret = os.environ['KEY_SECRET']
+
+# Relevant v2 Umbrella API endpoints
+base_url = "https://api.umbrella.com"
+access_token_endpoint = f"{base_url}/auth/v2/token"
+top_destinations_endpoint = f"{base_url}/reports/v2/top-destinations"
+
+# Generate new access token as these expire after 1 hour. Requires a valid and unexpired Umbrella API Key and Key Secret.
+def generate_access_token():
+
+    response = requests.post(url=access_token_endpoint,auth=(client_key,client_secret))
+    access_token = response.json()['access_token']
+
+    return access_token
+
+# Get the Top Destinations visited from 7 days ago until now. Top 100 domains are returned.
+def get_top_destinations(access_token):  
+    
+    headers = {
+    "Authorization": "Bearer " + access_token,
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    } 
+    
+    params = {
+        "from": "-7days",
+        "to": "now",
+        "offset": "0",
+        "limit": 100
+    }
+    
+    top_destinations_request = requests.get(top_destinations_endpoint, headers=headers,params=params)
+    top_destinations = top_destinations_request.json()
+
+    return top_destinations
+
+
+# Check that destination in Top Destinations is a domain.
+
+def isIP(str):
+    try:
+        IP(str)
+    except ValueError:
+        return False
+    return True 
+
+# If destination in Top Destinations is a domain, write it as a new line in a CSV called top_destinations.csv.
+def top_destinations_to_csv(top_destinations):
+
+    destinations_list = []
+
+    for destination in top_destinations['data']:
+        if not isIP(destination['domain']):
+            destinations_list.append(destination['domain'])
+
+    top_destinations_csvfile = open('top_destinations.csv', 'w')
+
+    with open('top_destinations.csv', 'w', newline='') as top_destinations_csvfile: 
+        filewriter = csv.writer(top_destinations_csvfile, delimiter=',',
+            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for destination in destinations_list: 
+            filewriter.writerow([destination])
+        top_destinations_csvfile.close()
+                
+    return top_destinations_csvfile
+            
+# Download the Umbrella top 1 million destinations, unzip file, format file. 
+def get_top_million():
+
+    # API call to get Umbrella Top 1 Million as a zip file
+    get_top_1million_zip = requests.get("http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip")
+
+    # Write the zip file to disk
+    open('top-1m.csv.zip', 'wb').write(get_top_1million_zip.content)
+
+    # Create a new CSV file to write the cleaned up Top 1 Million to
+    top_1million_csv = 'top-1m.csv'
+    
+    # Unzip the file
+    with zipfile.ZipFile('top-1m.csv.zip', 'r') as zip_ref: 
+        zip_ref.extractall('.')
+        
+    # Removing rank order in first column so that we can compare domains to Top Destinations. 
+    top_1million_csv = pandas.read_csv('top-1m.csv')
+    first_column = top_1million_csv.columns[0]
+    top_1million_csv = top_1million_csv.drop([first_column], axis=1)
+    top_1million_csv.to_csv('top_1million_csv', index=False)
+
+    return top_1million_csv
+    
+# Compares each domain in top_destinations.csv to top-1m.csv (Umbrella's Top 1 Million) and returns any domains that are not in the Top 1 Million.
+def find_uncommon_domains():
+
+    top_destinations_file_path = "./top_destinations.csv"
+    top_1million_file_path = "./top_1million_csv"
+        
+    uncommon_domains_file_path = "./uncommon_domains.csv"
+
+    with open(uncommon_domains_file_path, 'w') as uncommon_domains_csv:
+
+        top_destinations = open(top_destinations_file_path).readlines()
+        top_1million = open(top_1million_file_path).readlines()
+
+        for domain in top_destinations:
+            if domain not in top_1million: 
+                uncommon_domains_csv.write(domain)
+
+    print(f"Uncommon domains have been written to uncommon_domains.csv in your current directory.")
+
+    return uncommon_domains_csv
+
+# Clean up files used to determine uncommon domains.
+def clean_up_files():
+    os.remove('top-1m.csv.zip')
+    os.remove('top-1m.csv')
+    os.remove('top_destinations.csv')
+    os.remove('top_1million_csv')
+
+# Main function
+def main():
+
+    access_token = generate_access_token()
+    top_destinations = get_top_destinations(access_token)
+    top_destinations_csvfile = top_destinations_to_csv(top_destinations)
+    cleaned_top_1million_csv = get_top_million()
+    uncommon_domains_csv = find_uncommon_domains()
+    clean_up_files()
+
+    return uncommon_domains_csv
+
+if __name__ == "__main__":
+    main()
